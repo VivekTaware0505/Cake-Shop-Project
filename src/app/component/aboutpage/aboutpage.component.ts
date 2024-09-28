55f4961883f672425b4773add1a321aef280114b
vivek taware <vivektaware16@gmail.com>
2024-09-28 19:07:29 +0530

    create card for order cake create about page and create gett in touch page for user information create main content page

---
+++ src/app/component/aboutpage/aboutpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './aboutpage.component.html',
+  styleUrl: './aboutpage.component.css'
+})
+export class AboutpageComponent {
+
+}
---
+++ src/app/component/getintouch/getintouch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-getintouch',
+  standalone: true,
+  imports: [],
+  templateUrl: './getintouch.component.html',
+  styleUrl: './getintouch.component.css'
+})
+export class GetintouchComponent {
+
+}
