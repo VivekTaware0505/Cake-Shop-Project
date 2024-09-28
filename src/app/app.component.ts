753e7e8d229267b90e680d4b4f7e33d3566ed1d2
vivek taware <vivektaware16@gmail.com>
2024-09-28 19:56:15 +0530

    create footer and add some content and design get in touch page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from "./component/main/main.component";
 import { CardComponent } from "./component/card/card.component";
+import { AboutpageComponent } from "./component/aboutpage/aboutpage.component";
+import { GetintouchComponent } from "./component/getintouch/getintouch.component";
+import { FooterComponent } from "./component/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent, CardComponent],
+  imports: [RouterOutlet, MainComponent, CardComponent, AboutpageComponent, GetintouchComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
